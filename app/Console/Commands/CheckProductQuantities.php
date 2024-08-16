029a721fdaa08bdfea00210775d0b45281e7a3bd
Süleyman Denizhan <sdenizhan@windowslive.com>
2024-08-16 15:09:01 +0300

    reformat codes

--- app/Console/Commands/CheckProductQuantities.php
+++ app/Console/Commands/CheckProductQuantities.php
@@ -4,9 +4,11 @@ namespace App\Console\Commands;
 
 use App\Mail\ProductAlertMail;
 use App\Models\Product;
+use App\Models\Repo;
 use App\Models\RepoHasProducts;
 use App\Models\Setting;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class CheckProductQuantities extends Command
 {
@@ -27,41 +29,79 @@ class CheckProductQuantities extends Command
     /**
      * Execute the console command.
      */
+
     public function handle()
+    {
+        $alert = Setting::getGroup('alert');
+
+        if ( $alert['email_enabled'] ) {
+            $this->sendEmail($this->findLowProducts());
+        }
+
+        if ( $alert['sms_enabled']) {
+            $this->sendSms($this->findLowProducts());
+        }
+    }
+
+    public function findLowProducts()
     {
         // Get all products
         $products = Product::all();
 
-        //alert settings
-        $alert = Setting::getGroup('alert');
-
-        if ( $alert['email_enabled'] == 0 ) {
-            return;
-        }
+        $data = [];
 
         // Loop through each product
         foreach ($products as $product) {
-
             $repoHasProduct = RepoHasProducts::where(['product_id' => $product->id])->first();
 
             if ( $repoHasProduct ) {
 
+                if ( is_null($repoHasProduct->repo->name)) {
+                    continue;
+                }
+
                 if ( $repoHasProduct->quantity < $product->min_alert ) {
-
-                    $message = \Str::of($alert['email_message'])
-                        ->replace('{repo_name}', $repoHasProduct->repo->name ?? 'Unknown')
-                        ->replace('{product_name}', $product->name)
-                        ->replace('{product_count}', $repoHasProduct->quantity);
-
-                    // Send email
-                    $details = [
-                        'title' => $alert['email_subject'],
-                        'body' => $message
+                    $data[] = [
+                        'repo_name' => $repoHasProduct->repo->name,
+                        'product_name' => $product->name,
+                        'product_count' => $repoHasProduct->quantity
                     ];
-
-                    \Mail::to($alert['email_address'])->send(new ProductAlertMail($details));
                 }
             }
         }
+
+        return $data;
+    }
+
+    private function sendEmail(?array $products = [])
+    {
+        if (empty($products)) {
+            return;
+        }
+
+        $alert = Setting::getGroup('alert');
+
+        $table = '<table>';
+        $table .= '<tr><th>Repo Name</th><th>Product Name</th><th>Product Count</th></tr>';
+        foreach ($products as $product) {
+            $table .= '<tr>';
+            $table .= '<td>' . $product['repo_name'] . '</td>';
+            $table .= '<td>' . $product['product_name'] . '</td>';
+            $table .= '<td>' . $product['product_count'] . '</td>';
+            $table .= '</tr>';
+        }
+        $table .= '</table>';
+
+        $details = [
+            'title' => $alert['email_subject'],
+            'body' => Str::of($alert['email_message'])->replace('{table}', $table)
+        ];
+
+        \Mail::to($alert['email_address'])->send(new ProductAlertMail($details));
+    }
+
+    private function sendSms(?array $products = [])
+    {
+        return true;
     }
 }
--- resources/views/themes/backend/default/email-template/product-alert.blade.php
+++ resources/views/themes/backend/default/email-template/product-alert.blade.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel Mail</title>
+    <title>{{ $details['title'] }}</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        table, th, td, tr {
+            border: 1px solid #ddd;
+        }
+    </style>
 </head>
 <body>
 <h1>{{ $details['title'] }}</h1>
-<p>{{ $details['body'] }}</p>
 
-<p>Thank you!</p>
+<p>{!! $details['body'] !!}</p>
+
 </body>
 </html>
